f1b64f118c42c84481aed26006ebd813ad379374
Jeanine Santiago <jeanine.a.santiago@gmail.com>
2024-01-09 17:13:19 -0300

    Classe Smartphone

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,26 +1,38 @@
+using System;
+
 namespace DesafioPOO.Models
 {
+    // Classe abstrata que serve como base para os smartphones
     public abstract class Smartphone
     {
+        // Propriedades comuns a todos os smartphones
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo { get; set; }
+        public string IMEI { get; set; }
+        public int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        // Construtor da classe Smartphone, inicializa as propriedades comuns
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
+        // Método para ligar o smartphone
         public void Ligar()
         {
             Console.WriteLine("Ligando...");
         }
 
+        // Método para receber uma ligação
         public void ReceberLigacao()
         {
             Console.WriteLine("Recebendo ligação...");
         }
 
+        // Método abstrato para instalar um aplicativo, será implementado pelas subclasses
         public abstract void InstalarAplicativo(string nomeApp);
     }
 }
